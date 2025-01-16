197bf51064864409c4278392895c1f88f91cc0ef
laxminarasimhagude1995 <44166137+laxminarasimhagude1995@users.noreply.github.com>
2025-01-16 07:35:21 +0000

    data types done.

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "laxmi narasimha";
+let age = 28
+let marriageStatus = false
+let bodyCount = null
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof marriageStatus)
+console.log(typeof bodyCount)
+
+
