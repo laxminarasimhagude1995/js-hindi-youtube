65233d6ff06a5e272b7f057e18183e24714a22b6
laxminarasimhagude1995 <44166137+laxminarasimhagude1995@users.noreply.github.com>
2025-01-16 06:26:47 +0000

    variables learning.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345;
+let accountEmailId = "laxminarasimhagude@gmail.com";
+var accountEmailPassword = "Dream@11";
+accountCity = "Hyderabad";
+var accountState;
+// accountId = 23;
+accountEmailId = "girigude@gmail.com";
+accountEmailPassword = "Dream";
+accountCity = "Bombay";
+
+
+console.table([accountId,accountEmailId,accountEmailPassword,accountCity,accountState]);
+
+// const acts a constant and which value should not be changed.
+// let acts as a variable 
+// var acts as a variable
+// without  declaration of let and var 
+
+/*
+donot use the var because having issue in the scope 
+ */
